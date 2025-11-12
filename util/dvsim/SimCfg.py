a75abfc6d2a9e850e30e27ef4d9ffdd9ec22a0e2
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-11-12 16:32:40 +0000

    [dvsim] Enable opts for post_build commands
    dvsim already enables running commands after executing the
    `build` command with the option `post_build_cmds`.
    
    However, until now, it was not possible to pass options to this
    post build command.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- util/dvsim/SimCfg.py
+++ util/dvsim/SimCfg.py
@@ -103,6 +103,7 @@ class SimCfg(FlowCfg):
         self.flow_makefile = ""
         self.pre_build_cmds = []
         self.post_build_cmds = []
+        self.post_build_opts = []
         self.build_dir = ""
         self.pre_run_cmds = []
         self.post_run_cmds = []
@@ -251,11 +252,13 @@ class SimCfg(FlowCfg):
 
         # Walk through build modes enabled on the CLI and append the opts
         for en_build_mode in self.en_build_modes:
+            print("Mode: "+en_build_mode)
             build_mode_obj = find_mode(en_build_mode, self.build_modes)
             if build_mode_obj is not None:
                 self.pre_build_cmds.extend(build_mode_obj.pre_build_cmds)
                 self.post_build_cmds.extend(build_mode_obj.post_build_cmds)
                 self.build_opts.extend(build_mode_obj.build_opts)
+                self.post_build_opts.extend(build_mode_obj.post_build_opts)
                 self.pre_run_cmds.extend(build_mode_obj.pre_run_cmds)
                 self.post_run_cmds.extend(build_mode_obj.post_run_cmds)
                 self.run_opts.extend(build_mode_obj.run_opts)
@@ -375,9 +378,9 @@ class SimCfg(FlowCfg):
         # Merge the global build and run opts
         Test.merge_global_opts(self.run_list, self.pre_build_cmds,
                                self.post_build_cmds, self.build_opts,
-                               self.pre_run_cmds, self.post_run_cmds,
-                               self.run_opts, self.sw_images,
-                               self.sw_build_opts)
+                               self.post_build_opts, self.pre_run_cmds,
+                               self.post_run_cmds, self.run_opts,
+                               self.sw_images, self.sw_build_opts)
 
         # Process reseed override and create the build_list
         build_list_names = []
